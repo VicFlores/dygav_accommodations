39852fb93fc7fcea1404d5610653a3a5923bb206
Vic Flores <vicflores2211@gmail.com>
2025-02-11 10:35:50 -0600

    Refactor global styles, remove unused CSS, and add accommodation pages

---
+++ app/accommodation-details/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Accommodation Details',
+  description: 'Accommodation Details Page',
+  keywords: 'Accommodation, Details',
+};
+
+const AccommodationDDetailPage = () => {
+  return (
+    <div>
+      <h1>AccommodationsPage</h1>
+    </div>
+  );
+};
+
+export default AccommodationDDetailPage;
---
+++ app/accommodations/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Accomodations',
+  description: 'Accomodations for the wedding',
+  keywords: 'accomodations, wedding',
+};
+
+const AccommodationsPage = () => {
+  return (
+    <div>
+      <h1>AccommodationsPage</h1>
+    </div>
+  );
+};
+
+export default AccommodationsPage;
