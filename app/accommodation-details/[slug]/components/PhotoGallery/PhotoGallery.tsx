8b8236f05357b9e2b17a914c40d421f024fbd621
Vic Flores <vicflores2211@gmail.com>
2025-02-25 12:09:42 -0600

    Refactor PhotoGallery component to use grid layout for improved image display and remove unused code

--- app/accommodation-details/[slug]/components/PhotoGallery/PhotoGallery.tsx
+++ app/accommodation-details/[slug]/components/PhotoGallery/PhotoGallery.tsx
@@ -1,13 +1,9 @@
 'use client';
 
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import Image from 'next/image';
-import {
-  MdOutlineKeyboardArrowLeft,
-  MdOutlineKeyboardArrowRight,
-} from 'react-icons/md';
+
 import styles from './PhotoGallery.module.css';
-import { useRef } from 'react';
 
 interface PhotoGalleryProps {
   title: string;
@@ -22,84 +18,18 @@ export const PhotoGallery: FC<PhotoGalleryProps> = ({
   images,
   introductions,
 }) => {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrevClick = () => {
-    const isFirstImage = currentIndex === 0;
-
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-
-    if (scrollContainerRef.current) {
-      if (isFirstImage) {
-        // Scroll to the end
-        scrollContainerRef.current.scrollLeft =
-          scrollContainerRef.current.scrollWidth;
-      } else {
-        // Normal scroll to previous image
-        scrollContainerRef.current.scrollLeft -= 280 + 16; // image width + gap
-      }
-    }
-  };
-
-  const handleNextClick = () => {
-    const isLastImage = currentIndex === images.length - 1;
-
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-
-    if (scrollContainerRef.current) {
-      if (isLastImage) {
-        // Reset scroll position to the beginning
-        scrollContainerRef.current.scrollLeft = 0;
-      } else {
-        // Normal scroll to next image
-        scrollContainerRef.current.scrollLeft += 280 + 16; // image width + gap
-      }
-    }
-  };
-
-  const handleImageClick = (index: number) => {
-    setCurrentIndex(index);
-  };
-
   return (
     <section className={styles.container}>
       <h1 className={styles.title}>{title}</h1>
+
       <p className={styles.subtitle}>{subtitle}</p>
 
-      <div className={styles.galleryContainer}>
-        <figure className={styles.imageContainer}>
-          <Image
-            src={images[currentIndex]}
-            alt={`Foto ${currentIndex + 1}`}
-            fill
-          />
-        </figure>
-
-        <div className={styles.commingImageContainer}>
-          <MdOutlineKeyboardArrowLeft onClick={handlePrevClick} />
-
-          <div className={styles.imagesContainer} ref={scrollContainerRef}>
-            {images.map((image, index) => (
-              <figure
-                key={index}
-                className={`${styles.commingImage} ${
-                  index === currentIndex ? styles.active : ''
-                }`}
-                onClick={() => handleImageClick(index)}
-              >
-                <Image src={image} alt={`Foto ${index + 1}`} fill />
-              </figure>
-            ))}
-          </div>
-
-          <MdOutlineKeyboardArrowRight onClick={handleNextClick} />
-        </div>
+      <div className={styles.gallery}>
+        {images.slice(0, 5).map((image, index) => (
+          <figure key={index} className={styles.imageContainer}>
+            <Image src={image} alt='Image' fill className={styles.image} />
+          </figure>
+        ))}
       </div>
 
       <h1 className={styles.title}>Detalles de tu futuro alojamiento</h1>
